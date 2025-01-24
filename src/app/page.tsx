37538f16087a925b5f4bc04907547978f988a6ed
Cookiegigi <gcreusot@pm.me>
2025-01-24 15:23:43 +0800

    Center element and add photo place holder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+import Image from "next/image"
+
 export default function Home() {
   const handleDownload = async (key: string) => {
     try {
@@ -15,25 +18,38 @@ export default function Home() {
     }
   }
   return (
-    <div>
-      <h1>Guillaume Creusot</h1>
-      <h2>Software Engineer</h2>
-      #PHOTO#
-      <p>
-        Software engineer with 4 years of experience designing, implementing and managing web applications projects.
-        My last job principally involved technologies like ASP.Net, Microsoft SQL Server, JS/JQuery and HTML/CSS, but outside
-        of work, I’m an enthusiastic learner who likes to explore other languages such as Python and Rust or other domains
-        like self-hosting or Linux.
-        In my previous company, I was not only developer. I also had to deal with all aspect of a project including non-technical
-        one like project management and client need analysis.
-        I’m traveler who after traveling 9 months, search for a stable job in Malaysia.
-      </p>
-      <button
-        onClick={() => handleDownload("Guillaume_Creusot_CV.pdf")}
-        className="text-blue-500 hover:text-blue-600 transition duration-300"
-      >
-        Download
-      </button>
+    <div className="h-svh  flex flex-col justify-items-center items-center">
+      <div className="w-fit">
+        <div className="flex flex-row justify-center">
+          <h1>Guillaume Creusot</h1>
+        </div>
+        <div className="flex flex-row justify-center">
+          <h2>Software Engineer</h2>
+        </div>
+        <div className="flex flex-row justify-center">
+          <Image src="/photo-profile.png" alt="Photo de profil" width="200" height="200" />
+        </div>
+        <div className="flex flex-row justify-center">
+          <p>
+            Software engineer with 4 years of experience designing, implementing and managing web applications projects.
+            My last job principally involved technologies like ASP.Net, Microsoft SQL Server, JS/JQuery and HTML/CSS, but outside
+            of work, I’m an enthusiastic learner who likes to explore other languages such as Python and Rust or other domains
+            like self-hosting or Linux.
+            In my previous company, I was not only developer. I also had to deal with all aspect of a project including non-technical
+            one like project management and client need analysis.
+            I’m traveler who after traveling 9 months, search for a stable job in Malaysia.
+          </p>
+        </div>
+        <div className="flex flex-row justify-center">
+          <button
+            onClick={() => handleDownload("Guillaume_Creusot_CV.pdf")}
+            className="text-blue-500 hover:text-blue-600 transition duration-300"
+          >
+            Download
+          </button>
+        </div>
+      </div>
     </div>);
+
 
 }
