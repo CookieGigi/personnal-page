03bfbe7c6865998ab9980d9cfe6349cc3ff45b6a
Cookiegigi <gcreusot@pm.me>
2025-01-24 12:10:43 +0800

    Add basic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-
 export default function Home() {
   return (
+    <div>
+      <h1>Guillaume Creusot</h1>
+      <h2>Software Engineer</h2>
+      #PHOTO#
+      <p>
+        Software engineer with 4 years of experience designing, implementing and managing web applications projects.
+        My last job principally involved technologies like ASP.Net, Microsoft SQL Server, JS/JQuery and HTML/CSS, but outside
+        of work, I’m an enthusiastic learner who likes to explore other languages such as Python and Rust or other domains
+        like self-hosting or Linux.
+        In my previous company, I was not only developer. I also had to deal with all aspect of a project including non-technical
+        one like project management and client need analysis.
+        I’m traveler who after traveling 9 months, search for a stable job in Malaysia.
+      </p>
+      <button>CV</button>
+    </div>);
 
 }
